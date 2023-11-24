fc4daf45ff9a49d68d78101fe6af1da0e351e2d7
Pendekanti Saikarthik <144220901+SaikarthikPendekanti@users.noreply.github.com>
2023-11-24 12:49:51 +0530

    Update Home.js

--- SignUp_Frontend-main/src/components/Home.js
+++ SignUp_Frontend-main/src/components/Home.js
@@ -3,7 +3,7 @@ import  { useEffect } from "react";
 function Home() {
   useEffect(() => {
     // Redirect to http://localhost:3001/ when the component mounts
-    window.location.href = "https://srirampodcast.vercel.app/";
+    window.location.href = "https://mern-project-final-ivory.vercel.app/";
   }, []); // Empty dependency array ensures the effect runs only once when the component mounts
 
   // This component doesn't render anything since it redirects immediately
